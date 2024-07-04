40f266b8be3530b5278f98ab10cd6be07d1855d4
Vasiliy <cenetik@gmail.com>
2024-07-05 00:29:12 +1000

    Добавил проект тестов и один тест

---
+++ UnitTests/Services/PatientServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using App.Exceptions;
+using App.Extensions;
+using App.Models;
+using App.Services;
+using DataAccess.RepositoryImpls;
+using Domain;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.Services
+{
+    public class PatientServiceTests
+    {     
+        [Fact]
+        public void SavePatient_SocNumberNotNumberic_ThrowExceptioin()
+        {
+            var fakeDataFactory = new FakeDataFactory();
+            var patientRepository = new InMemoryBaseRepository<Patient>(fakeDataFactory.Patients);
+            var visitRepository = new InMemoryBaseRepository<Visit>(fakeDataFactory.Visits);
+            var patientsService = new PatientsService(patientRepository,visitRepository);
+
+            var patient = fakeDataFactory.Patients.First().ToDto();
+
+            patient.SocialSecurityNumber = "2222Y1111";
+
+            var exception = Assert.Throws<ValidateException>(() => patientsService.Edit(patient));
+            Assert.Equal("Номер полиса содержит недопустимые символы! Допустимы только цифры без пробелов!", exception.Message);
+        }
+    }
+}
